687db89817525a4102f7156e3d45a68a416582d4
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-09 21:42:43 +0100

    register 3 nav

--- functions.php
+++ functions.php
@@ -7,6 +7,14 @@ add_theme_support('post-thumbnails');
 add_theme_support('title-tag');
 
 register_nav_menus(array(
-    'main' => 'Menu Principal',
+    'main-top' => 'Menu Principal Haut',
+    'main-bottom' => 'Menu Principal Bas',
     'footer' => 'Bas de page',
 ));
+
+wp_enqueue_style(
+    'main',
+    get_template_directory_uri() . '/css/main.css',
+    array(),
+    '1.0'
+);
