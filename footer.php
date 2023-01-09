d3857fd1e3b12e9b68c018a4123b5b74decabe34
Frédéric Bui <bui.frederic.pro@gmail.com>
2023-01-09 21:43:19 +0100

    include nav in footer and header

--- footer.php
+++ footer.php
@@ -1,3 +1,4 @@
+</main>
 <footer class="site__footer">
     <?php wp_nav_menu(array('theme_location' => 'footer')); ?>
 </footer>
--- header.php
+++ header.php
@@ -11,16 +11,34 @@
 <body <?php body_class(); ?>>
     <header class="header">
         <a href="<?php echo home_url('/'); ?>">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="Logo">
+            <img src="<?php echo get_template_directory_uri(); ?>/logo.png" alt="Logo">
         </a>
-        <?php
-        wp_nav_menu(
-            array(
-                'theme_location' => 'main',
-                'container' => 'ul', // afin d'éviter d'avoir une div autour 
-                'menu_class' => 'site__header__menu', // ma classe personnalisée 
-            )
-        );
-        ?>
+        <div id="main-menu">
+            <div id="main-menu-top">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'main-top',
+                        'container' => 'ul', // afin d'éviter d'avoir une div autour 
+                        'menu_class' => 'site__header__menu', // ma classe personnalisée 
+                    )
+                );
+                ?>
+            </div>
+            <div id="main-menu-bottom">
+                <?php
+                wp_nav_menu(
+                    array(
+                        'theme_location' => 'main-bottom',
+                        'container' => 'ul', // afin d'éviter d'avoir une div autour 
+                        'menu_class' => 'site__header__menu', // ma classe personnalisée 
+                    )
+                );
+                ?>
+            </div>
+        </div>
+
     </header>
-    <?php wp_body_open(); ?>
+    <div id="space"></div>
+    <main>
+        <?php wp_body_open(); ?>
