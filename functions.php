f3db74b71ae51f85cda0c3fd50db67f093e279e4
Frederic Bui <bui.frederic.pro@gmail.com>
2023-02-12 17:14:12 +0100

    change articles to publications

--- functions.php
+++ functions.php
@@ -18,3 +18,24 @@ wp_enqueue_style(
     array(),
     '1.0'
 );
+
+add_action('init', 'cp_change_post_object');
+// Change dashboard Posts to News
+function cp_change_post_object()
+{
+    $get_post_type = get_post_type_object('post');
+    $labels = $get_post_type->labels;
+    $labels->name = 'Publications';
+    $labels->singular_name = 'Publications';
+    $labels->add_new = 'Ajouter une Publications';
+    $labels->add_new_item = 'Ajouter une Publications';
+    $labels->edit_item = 'Editer une Publications';
+    $labels->new_item = 'Publications';
+    $labels->view_item = 'Voir une Publications';
+    $labels->search_items = 'Rechercher une Publications';
+    $labels->not_found = 'Pas de Publications trouvées';
+    $labels->not_found_in_trash = 'No Publications found in Trash';
+    $labels->all_items = 'Toutes les Publications';
+    $labels->menu_name = 'Publications';
+    $labels->name_admin_bar = 'Publications';
+}
